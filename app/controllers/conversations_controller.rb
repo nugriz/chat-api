2877cbda83387e2fb1ac7feb63809a1b33422429
nugriz <rizkinug02@gmail.com>
2021-07-23 15:11:47 +0700

    Destroy versioning

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,29 @@
+class ConversationsController < ApplicationController
+
+    # GET /conversations
+  def index
+    conversations_from_friends = Conversation.find_by!(friend: current_user.phone) 
+    @conversations = current_user.conversations.all
+    @conversations.merge(conversations_from_friends)
+    json_response(@conversations)
+  end
+
+    # POST /conversations
+  def create
+    @conversation = Conversation.create!(conversation_params)
+    json_response(@conversation, :created)
+  end
+
+    # GET /conversations/:id
+  def show
+    @conversation = Conversation.find(params[:id])
+    json_response(@conversation)
+  end
+
+  private
+
+  def conversation_params
+      # whitelist params
+    params.permit(:friend)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 
-  scope module: :v1, constraints: ApiVersion.new('v1', true) do
-    resources :conversations do
-      resources :messages
-    end
+  resources :conversations do
+    resources :messages
   end
 end
